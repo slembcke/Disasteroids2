ed43f03889c36a501cf8d1f104408f875321db6e
slembcke <slembcke@gmail.com>
2013-10-17 15:21:02 -0500

    Initial import

---
+++ Disasteroids2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Disasteroids2
+//
+//  Created by Scott Lembcke on 10/17/13.
+//  Copyright (c) 2013 Cocos2D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Disasteroids2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Disasteroids2
+//
+//  Created by Scott Lembcke on 10/17/13.
+//  Copyright (c) 2013 Cocos2D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
